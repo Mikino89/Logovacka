3afe04ba6396b19554dc85c1dbca8460c4124827
Mikino <mikino89@gmail.com>
2014-12-20 11:14:51 +0100

    php kontrola formulara

--- data/vloz.php
+++ data/vloz.php
@@ -11,35 +11,23 @@ if (!isset($_POST['datum']) || !isset($_POST['cas']) || !isset($_POST['meno']) |
 
 $datum = date_format(new DateTime($_POST['datum']), 'Y-m-d');
 $cas = $_POST['cas'];
-$meno= htmlspecialchars($_POST['meno']);
-$telefon = htmlspecialchars($_POST['telefon']);
-$email = htmlspecialchars($_POST['email']);
+$meno= trim($_POST['meno']);
+$telefon = str_replace(' ', '', $_POST['telefon']);
+$email = trim($_POST['email']);
 $color = '';
 $datum_cas[] = '';
+$stv = 1;
 
 if(empty($datum) || empty($cas) || empty($meno) || empty($telefon) || empty($email))
     die("error");
-/*
-function isEmail($to_validate)
-{
-  $RegExp ="/^([a-zA-Z0-9_\-])+(\.([a-zA-Z0-9_\-])+)*@((\[(((([0-1])?";
-  $RegExp.="([0-9])?[0-9])|(2[0-4][0-9])|(2[0-5][0-5])))\.(((([0-1])?";
-  $RegExp.="([0-9])?[0-9])|(2[0-4][0-9])|(2[0-5][0-5])))\.(((([0-1])?";  
-  $RegExp.="([0-9])?[0-9])|(2[0-4][0-9])|(2[0-5][0-5])))\.(((([0-1])?"; 
-  $RegExp.="([0-9])?[0-9])|(2[0-4][0-9])|(2[0-5][0-5]))\]))|";
-  $RegExp.="((([a-zA-Z0-9])+(([\-])+([a-zA-Z0-9])+)*\.)+([a-zA-Z])+";
-  $RegExp.= "(([\-])+([a-zA-Z0-9])+)*))$/";
-  return preg_match($RegExp,$to_validate);
-}
 
-if(isEmail($email) == 0){
-    echo('error');
-}
-*/
-$query = "INSERT INTO zaznamy (datum, cas, meno, telefon, email, stav) VALUES('$datum', '$cas', '$meno', '$telefon', '$email', 1)";
-$ok = mysqli_query($mysqli, $query);
+    $zapis = mysqli_prepare($mysqli, "INSERT INTO zaznamy (datum, cas, meno, telefon, email, stav) VALUES(?, ?, ?, ?, ?, ?)");
+    mysqli_stmt_bind_param($zapis, 'sssssi', $datum, $cas, $meno, $telefon, $email, $stv);
+    mysqli_stmt_execute($zapis);
 
-if ($ok)
+
+
+if ($zapis)
     echo "ok";
 else
     echo "error";
